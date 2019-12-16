046370a1790e67851fe1b4317f1079ac60724a77
Aidan Lincoln <aidantlincoln@gmail.com>
2019-12-16 07:20:18 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -13,7 +13,7 @@ def join_ingredients(src)
   while row_index < src.count
     element_index = 0
     while element_index < src[row_index].count
-      array.push(`I love ${}`)  
+      array.push(`I love #{src[row_index][0]} and #{}`)  
 end
 
 def find_greater_pair(src)
